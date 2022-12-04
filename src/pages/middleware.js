ed56f50745b863f9cc25d9644994be86accb3149
seakh-masum <masum0aktar@gmail.com>
2022-12-04 12:39:35 +0530

    firebase hosting added

--- src/pages/middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse, NextRequest } from 'next/server'
-import { checkCookie } from 'shared/helper/storage'
-export async function middleware(req, ev) {
-  const { pathname } = req.nextUrl
-  if (pathname !== '/auth' && checkCookie('user')) {
-    return NextResponse.redirect('/auth/login')
-  }
-  return NextResponse.next()
-}
